e1db3d13f7ed45971d4ee52d9d6333ba92f3be08
Antony Budianto <antonybudianto@gmail.com>
2018-02-16 13:08:31 +0700

    Init routing

---
+++ client/src/routes/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './LandingPage.css';
+
+const LandingPage = () => (
+  <main role="main">
+    <section className="jumbotron text-center">
+      <div className="container">
+        <h1 className="jumbotron-heading">RestoHub</h1>
+        <p className="lead text-muted">Book your nearest restaurant instantly</p>
+      </div>
+    </section>
+
+  </main>
+);
+
+export default LandingPage;
