52c866b6e110354288c80e80f7f04144cf2993c1
Antony Budianto <antonybudianto@gmail.com>
2018-02-17 08:57:43 +0700

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 import LandingPage from './routes/LandingPage';
 
 import './App.css';
@@ -9,11 +10,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" id="app">
         <Header />
         <Switch>
           <Route path="/" exact component={LandingPage} />
         </Switch>
+        <Footer />
       </div>
     );
   }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#app">Back to top</a>
+      </p>
+      <p>&copy; 2018 RestoHub.</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
